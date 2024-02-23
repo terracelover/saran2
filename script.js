48a7fe03e8c206b8c9b265ee614846139d51ff59
terracelover <160361065+terracelover@users.noreply.github.com>
2024-02-24 00:46:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function openYesPage() {
   // Replace 'https://example.com' with the URL of the webpage you want to open
-  window.open('https://terracelover.github.io/Formysweetboy/');
+  window.open('https://terracelover.github.io/middlepage//');
 }
